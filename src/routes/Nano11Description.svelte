94e94a5b4c1c433cea033667375ea1fdded8daf5
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2023-10-15 19:22:48 +0200

    Update Nano11Description.svelte

--- src/routes/Nano11Description.svelte
+++ src/routes/Nano11Description.svelte
@@ -38,8 +38,8 @@
 	<TextBlock variant="titleLarge">Download</TextBlock>
 	<br />
 	<div id="buttons">
-		<Button variant="accent" href="https://archive.org/details/nano11-v4-open-beta">Latest build (V4 Open Beta)</Button>
-		<Button variant="accent" href="https://archive.org/details/nano11-v3">Latest stable build (V3)</Button>
+		<Button variant="accent" href="https://archive.org/details/nano11-23h2-v1-rp">Latest build (23H2 V1 RP)</Button>
+		<Button variant="accent" href="https://archive.org/details/nano11-v4-rtm">Latest stable build (V4)</Button>
 		<Button variant="accent" href="https://mega.nz/folder/8StwhBAQ#_ZBA8U-t5tf_XYna_C-2LA">Get Nano11 Extras</Button>
 		<Button variant="accent" href="https://github.com/XPower7125/nano11-post-setup">Post-Setup Repo</Button>
 		<Button variant="accent" href="https://discord.gg/u6tHeZJe">More in our Discord</Button>
