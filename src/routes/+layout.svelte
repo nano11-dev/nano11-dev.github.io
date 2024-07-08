64f8d3c2dd1b511d27cc08fc87e5834673fc71e3
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2024-07-08 10:06:12 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <nav>
 	<Button href="/" variant="hyperlink">Home</Button>
 	<Button href="/blog" variant="hyperlink">Blog</Button>
-	<Button href="https://www.youtube.com/redirect?event=comments&redir_token=QUFFLUhqbnNSb19oM0dLblA5NFUtQzZQM2dkN3dNTkdHUXxBQ3Jtc0tsZFdQWmM2bVlheVZYUDg4R2tqN1FPSmEzMGhkbEh4SVNxUkk4S0NtUkRtM1lMd05wa3BZN3RSR2gzLVFvWnNac0lfMjBfUXBLSW1XbkJkWjc0bS1PSGplczBJbllPekVVaWtRaWc2a3JrU0FINHFobw&q=https%3A%2F%2Fdrive.google.com%2Ffile%2Fd%2F1rzUBwvj9qNr07MjzF8z1F6oJLOCn8UGF%2Fview%3Fusp%3Ddrive_link " variant="hyperlink">Download latest version</Button>
+	<Button href="https://drive.google.com/file/d/1rzUBwvj9qNr07MjzF8z1F6oJLOCn8UGF/view?usp=drive_link " variant="hyperlink">Download latest version</Button>
 </nav>
 
 <slot />
