7aae5c95f0e3b59f1bc8b57f71ab5bf3bf9cb1c2
StarNumber <starnumber.official12046@gmail.com>
2024-07-11 09:12:53 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <nav>
 	<Button href="/" variant="hyperlink">Home</Button>
 	<Button href="/blog" variant="hyperlink">Blog</Button>
-	<Button href="https://drive.google.com/file/d/1rzUBwvj9qNr07MjzF8z1F6oJLOCn8UGF/view?usp=drive_link " variant="hyperlink">Download latest version</Button>
+	<Button href="https://drive.google.com/file/d/1rzUBwvj9qNr07MjzF8z1F6oJLOCn8UGF/view?usp=drive_link" variant="hyperlink">Download latest version</Button>
 </nav>
 
 <slot />
